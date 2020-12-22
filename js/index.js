689126ad6fba0bb982c81492a75561b44ea3b3c2
akim-a <faino.ak@gmail.com>
2020-12-22 19:12:58 +0000

    Sample code

---
+++ js/index.js
@@ -0,0 +1,46 @@
+function updateClock() {
+    const clock = document.querySelector('.clock');
+    clock.innerText = new Date().toLocaleTimeString();
+}
+setInterval(updateClock, 1000);
+
+
+{
+    const images = [
+        'img/amg.jpg',
+        'img/auto-wash.jpg',
+        'img/car-detailing',
+        'img/car-wash-pro/jpeg'
+    ]
+
+    let currentIdx = 0;
+    function showCurrent() {
+        const imgElement1 = document.querySelector('.carousel3 .img1');
+        const imgElement2 = document.querySelector('.carousel3 .img2');
+        const imgElement3 = document.querySelector('.carousel3 .img3');
+        const idx2 = currentIdx + 1 >= images.length ? 0 : currentIdx + 1;
+        const idx3 = idx2 + 1 >= images.length ? 0 : idx2 + 1;
+        console.log(currentIdx, idx2, idx3);
+        imgElement1.src = images[currentIdx];
+        imgElement2.src = images[idx2];
+        imgElement3.src = images[idx3];
+    }
+
+    function showNext() {
+        currentIdx++;
+        if (currentIdx >= images.length) currentIdx = 0;
+        showCurrent();
+    }
+
+    function showPrev() {
+        currentIdx--;
+        if (currentIdx < 0) currentIdx = images.length - 1;
+        showCurrent();
+    }
+
+    setInterval(showNext, 3000);
+
+    document.querySelector('.carousel3 .next').addEventListener('click', showNext);
+    document.querySelector('.carousel3 .prev').addEventListener('click', showPrev);
+    showCurrent();
+}
